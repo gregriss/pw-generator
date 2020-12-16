f068cde3eda2ba292d0a9860585c19e4ad5979e6
Greg Riss <gregriss23@gmail.com>
2020-12-15 22:07:39 -0500

    adding some pseudocode from acceptance criteria

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 // Assignment Code
+
+
+// AS AN employee with access to sensitive data
+// I WANT to randomly generate a password that meets certain criteria
+// SO THAT I can create a strong password that provides greater security
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+//THEN I choose lowercase, uppercase, numeric, and/or special characters
+// confirm if they want lowercase
+// confirm if they want uppercase
+// confirm if they want numeric
+// confirm if they want special characters
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// we need to show an error if they gave us no characters to choose from
+
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// for all the characters chosen, use one randomly and add it to our password x number of times
+// 
+
+
+
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
